68519c98cc2b17964f5b5b6d49bd053540833b60
Cody Johns <cjohns365@gmail.com>
2024-01-21 18:25:16 -0700

    Updated bio and project image

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -3,18 +3,15 @@
     <div class="col-sm-6 offset-sm-3">
         <h2>{{ getGreetingByTimeOfDay }}!</h2>
         <p>
-            I'm Cody Johns, Software Engineer and future BYU Graduate.
+            I'm Cody Johns, Software Engineer graduating from BYU in April 2024.
         </p>
         <p>
-            AI code generators are great, but you know what's even better than AI generated code? Someone who knows how to develop headache-free software that will work 100% of the time and will always be maintainable. 
-            No need to spend hours talking to a LLM that might or might not know what you need.
+            In my professional work, coursework, and personal projects I closely follow the principle of developing maintainable software. The software 
+            that a company uses can make or break their business. That's why hiring the right software developers is a crucial task. A company needs reliable/trustworthy, knowledgeable, 
+            and hardworking software developers. This is what I can offer and more.
         </p>
-        <p> Don't let the robots win in the end.</p>
         <p>
             As a programmer who started in his high school days, I am passionate about what I do. I've programmed microcontrollers, Raspberry Pis, computer games, to websites and mobile smartphone apps.
-        </p>
-        <p>
-            I'm more than qualified for your project.
         </p>
         <p>Here's some of the tools I've used:</p>
         <div class="row pb-4">
@@ -25,6 +22,7 @@
                     <li>HTML & CSS</li>
                     <li>React.JS</li>
                     <li>React Native</li>
+                    <li>Flutter</li>
                     <li>Vue.JS</li>
                     <li>Jenkins</li>
                     <li>Docker</li>
@@ -42,6 +40,7 @@
                     <li>Nginx</li>
                     <li>Apache</li>
                     <li>MySQL</li>
+                    <li>PostgreSQL Vectors for LLMs</li>
                 </ul>
             </div>
         </div>
